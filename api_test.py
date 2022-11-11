f5f5da6d83d05bb1fa9c778612407afeac353ec5
Aniket Thani <76532702+Aniketthani@users.noreply.github.com>
2022-11-11 23:41:57 +0530

    Add files via upload

---
+++ api_test.py
@@ -0,0 +1,40 @@
+import requests
+import json
+
+local_url = "http://localhost:7071/api/mlmodel"
+azure_url = "YOUR_AZURE_URL_PLUS_CODE"
+
+data = [
+    {
+    'pclass': '1',
+    'age': 29,
+    'embarked': 'C',
+    'sex': 'female'
+    },
+    {
+    'pclass': '1',
+    'age': 29,
+    'embarked': 'C',
+    'sex': 'male'
+    },
+    {
+    'pclass': '2',
+    'age': 29,
+    'embarked': 'C',
+    'sex': 'female'
+    },
+    {
+    'pclass': '2',
+    'age': 29,
+    'embarked': 'C',
+    'sex': 'male'
+    }
+]
+
+r = requests.post(local_url, json=json.dumps(data))
+print(r)
+print(r.text)
+
+#r = requests.post(azure_url, json=json.dumps(data))
+#print(r)
+#print(r.text)
